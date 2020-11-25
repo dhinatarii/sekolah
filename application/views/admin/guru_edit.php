5efb515128ada284d284d1ea8c29c4c9014f9a13
Taufik <taufik.amary@gmail.com>
2020-11-25 17:53:59 +0700

    add triger to user

--- application/views/admin/guru_edit.php
+++ application/views/admin/guru_edit.php
@@ -30,6 +30,11 @@
                     <?php echo form_error('jenis_kelamin', '<div class="text-danger small ml-3">', '</div>') ?>
                 </div>
                 <div class="form-group">
+                    <label for="tanggal_lahir">Tanggal Lahir</label>
+                    <input placeholder="Masukan Tanggal Lahir" type="text" class="form-control datepicker" name="tanggal_lahir" id="tanggal_lahir" value="<?php echo $guru['tanggal_lahir']; ?>">
+                    <?php echo form_error('tanggal_lahir', '<div class="text-danger small ml-3">', '</div>') ?>
+                </div>
+                <div class="form-group">
                     <label for="no_hp">No Handphone</label>
                     <input type="text" name="no_hp" id="no_hp" placeholder="Masukan No Handphone" class="form-control" value="<?php echo $guru['no_hp']; ?>">
                     <?php echo form_error('no_hp', '<div class="text-danger small ml-3">', '</div>') ?>
@@ -74,3 +79,13 @@
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
+
+<script>
+    $(function() {
+        $(".datepicker").datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            todayHighlight: true,
+        });
+    });
+</script>
